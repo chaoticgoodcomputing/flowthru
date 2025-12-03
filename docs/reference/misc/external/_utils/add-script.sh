32445d5f3c47a5cfda3c1ef72ec3d553fa05d64c
Spencer Elkington <spelkington@gmail.com>
2025-12-02 22:40:35 -0700

    fix: minor updates

--- docs/reference/misc/external/_utils/add-script.sh
+++ docs/reference/misc/external/_utils/add-script.sh
@@ -43,31 +43,6 @@ SOURCE_TYPE=repo
 SOURCE_ADDRESS=$SOURCE_URL
 EOF
 
-# Create template README.md
-cat > "$SOURCE_DIR/README.md" << 'EOF'
-# External Source Notes
-
-<!-- Brief summary of the source itself, including the category of the source -->
-
-## Key Takeaways
-
-<!-- An ordered list of key aspects of the source. In an external code source, this may be the overall structure of the project or the technical stack it uses. In external tool documentation, it may be use cases of the tool or key features the tool offers. In philosophy and design docs, it may be key tenants of the philosophy being analyzed -->
-
-## Applications of Takeaways
-
-### Takeaway 1
-
-<!-- Begin the section expanding on details from the source that relate to this key takeaway -->
-
-#### Connections
-
-<!-- Using other external source notes, answer the question: How does this connect to other external documentation? -->
-
-#### Applications
-
-<!-- Using context into the main project, answer the question: How does this help us develop the main project better? -->
-EOF
-
 echo "Directory structure created successfully"
 
 # Automatically pull the repository
