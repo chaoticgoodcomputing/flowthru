a654c16e26549994ea0fa075d6d2d193d8d9c0a6
Spencer Elkington <spelkington@gmail.com>
2026-04-23 13:37:38 -0600

    fix: resolve KedroSpaceflightsCustom config settings

--- examples/advanced/KedroSpaceflights.Custom/Data/FlowConfig.cs
+++ examples/advanced/KedroSpaceflights.Custom/Data/FlowConfig.cs
@@ -12,10 +12,10 @@ namespace KedroSpaceflights.Custom.Data;
 public partial class FlowConfig
 {
   /// <summary>Configuration options for the train/test split step.</summary>
-  [ConfigSection("Flowthru:Flows:DataScience")]
+  [ConfigSection("Flowthru:Flows:DataScience:ModelParams")]
   public partial IItem<CreateTestTrainSplitStep.TestTrainSplitParams> ModelParams { get; }
 
   /// <summary>Configuration options for cross-validation.</summary>
-  [ConfigSection("Flowthru:Flows:DataEvaluation")]
+  [ConfigSection("Flowthru:Flows:DataEvaluation:CrossValidationParams")]
   public partial IItem<CrossValidateModelStep.Params> CrossValidationParams { get; }
 }
